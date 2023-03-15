af7f79a7879765449a5e3f24f6251e8ae02fecc6
Daniel Falcão <daniel.falcao@triasoftware.com.br>
2023-03-15 19:57:49 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic6-login-with-menu',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
